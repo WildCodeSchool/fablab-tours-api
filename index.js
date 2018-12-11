99ea7eb325bf99f141538c7021786934be887b26
musloda <mus_tapha79@outlook.com>
2018-12-11 17:52:19 +0100

    envois api google calendar

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const oAuth2 = require('./oAuth');
 const port = 3000;
 const connection = require('./configuration');
 const bodyParser = require('body-parser');
+
+const { google } = require('googleapis');
 
 //middleware
 app.use((req, res, next) => {
@@ -26,6 +29,41 @@ app.get('/api/machines', (req, res) => {
         }
     });
 });
+
+// api calendrier
+function listEvents(auth) {
+    return new Promise(function (resolve, reject) {
+  
+      const calendar = google.calendar({ version: 'v3', auth });
+      calendar.events.list({
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime',
+      }, (err, res) => {
+        if (err) {
+          reject(err); 
+          return;
+        }
+        resolve(res.data.items);
+  
+      });
+    });
+  }
+  
+  app.get('/api/calendar/events', function (req, res) {
+    oAuth2.getToken((token) => {
+      listEvents(token)
+      .then((events) => {
+        res.json(events);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      })
+    })
+  });
+  
 
 
 app.listen(port, (err) => {
