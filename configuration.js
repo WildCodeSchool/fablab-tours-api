6f7416d5aaa60ba4aad13bbd3d1df11c1e0dbb0e
bloubna <38813329+bloubna@users.noreply.github.com>
2018-12-11 15:03:04 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -3,6 +3,6 @@ const connection = mysql.createConnection({
     host: 'localhost', // adresse du serveur
     user: 'root', // le nom d'utilisateur
     password: '', // le mot de passe
-    database: 'funlab', // le nom de la base de données
+    database: '', // le nom de la base de données
 });
 module.exports = connection;
