162ee28dd9d56e0a06ddc4356e0c9a2be7881cfc
maxdhn <maxduchan@gmail.com>
2019-01-23 11:39:01 +0100

    winston logger

--- configuration/environment.js
+++ configuration/environment.js
@@ -9,6 +9,7 @@ const DB_HOST = process.env.DB_HOST ? process.env.DB_HOST : 'localhost';
 const DB_PASSWORD = process.env.DB_PASSWORD ? process.env.DB_PASSWORD : 'jecode4wcs';
 const DB_USER = process.env.DB_USER ? process.env.DB_USER : 'root';
 const DB_PORT = process.env.DB_PORT ? process.env.DB_PORT : '3306';
+const DB_POOL_LIMIT = process.env.DB_POOL_LIMIT ? process.env.DB_POOL_LIMIT : 10;
 const DB_URI = process.env.DB_URI ? process.env.DB_URI : '';
 
 const MAILER_SERVICE = process.env.MAILER_SERVICE ? process.env.MAILER_SERVICE : 'gmail';
@@ -19,6 +20,8 @@ const MAILCHIMP_API_KEY =  process.env.MAILCHIMP_API_KEY ? process.env.MAILCHIMP
 const OAUTH2_KEY_PATH = process.env.OAUTH2_KEY_PATH ? path.resolve(path.normalize(process.env.OAUTH2_KEY_PATH)) : path.resolve(process.cwd(), path.normalize('.credentials/oauth2-client-id.json'));
 const OAUTH2_TOKEN_PATH = process.env.OAUTH2_TOKEN_PATH ? path.resolve(path.normalize(process.env.OAUTH2_TOKEN_PATH)) : path.resolve(process.cwd(), path.normalize('.credentials/oauth2-token.json'));
 
+const LOG_FILE_PATH = process.env.LOG_FILE_PATH ?process.env.LOG_FILE_PATH : 'logs';
+
 module.exports.NODE_PORT = NODE_PORT;
 module.exports.JWT_SECRET = JWT_SECRET;
 module.exports.DB = DB;
@@ -26,6 +29,7 @@ module.exports.DB_HOST = DB_HOST;
 module.exports.DB_PASSWORD = DB_PASSWORD;
 module.exports.DB_USER = DB_USER;
 module.exports.DB_PORT = DB_PORT;
+module.exports.DB_POOL_LIMIT = DB_POOL_LIMIT;
 module.exports.DB_URI = DB_URI;
 module.exports.MAILER_SERVICE = MAILER_SERVICE;
 module.exports.MAILER_USER = MAILER_USER;
@@ -33,3 +37,4 @@ module.exports.MAILER_PASSWORD = MAILER_PASSWORD;
 module.exports.MAILCHIMP_API_KEY = MAILCHIMP_API_KEY;
 module.exports.OAUTH2_KEY_PATH = OAUTH2_KEY_PATH;
 module.exports.OAUTH2_TOKEN_PATH = OAUTH2_TOKEN_PATH;
+module.exports.LOG_FILE_PATH = LOG_FILE_PATH;
